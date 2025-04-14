961ae3386eadd5dc1dd661c8989f01eb539f5a1e
dg0mezm <dg0mezm@outlook.es>
2025-04-14 20:08:54 +0200

    style: clean up whitespace

--- orvus.py
+++ orvus.py
@@ -9,13 +9,11 @@ def parse_arguments():
     parser.add_argument("-i", "--ip", type=str, required=True, help="Target IP Address")
     parser.add_argument("--ignore-ping", type=bool, default=False, help="Indicates if ignore initial ping.")
     parser.add_argument("--debug", type=bool, default=False, help="Toggle debug mode")
-    
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-
     Orvus(args)
 
 
